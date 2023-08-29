a39b1467bcf268a162263e84cfa3cb8a9d08e3af
aman-icap <atg2156@cumc.columbia.edu>
2023-08-29 14:23:40 +0300

    adding Ethiopian calendar phase 1

--- packages/esm-patient-registration-app/webpack.config.js
+++ packages/esm-patient-registration-app/webpack.config.js
@@ -1,1 +1,6 @@
-module.exports = require('openmrs/default-webpack-config');
+const config = (module.exports = require('openmrs/default-webpack-config'));
+// Overrides to disable CSS Modules for non-scss scripts, this means
+// CSS Modules will only be supported with .scss scripts
+config.cssRuleConfig.use = ['style-loader', 'css-loader'];
+config.cssRuleConfig.test = /\.css$/;
+module.exports = config;
